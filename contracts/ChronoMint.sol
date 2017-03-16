c5bc803d80bec8a53c3d5b2405c25ea01363f286
Mike Fluff <mikefluff@mikefluff.com>
2017-03-16 20:07:28 +0700

    MINT-87 remLOC, updLOCStatus, updLOCValue events have been added

--- contracts/ChronoMint.sol
+++ contracts/ChronoMint.sol
@@ -9,6 +9,9 @@ contract ChronoMint is Managed {
   mapping(uint => address) offeringCompanies;
   mapping(address => uint) offeringCompaniesIDs;
   event newLOC(address _from, address _LOC);
+  event remLOC(address _from, address _LOC);
+  event updLOCStatus(address _from, address _LOC);
+  event updLOCValue(address _from, address _LOC);
 
   function init(address _userStorage, address _shareable) {
     userStorage = _userStorage;
@@ -19,11 +22,13 @@ contract ChronoMint is Managed {
      offeringCompanies[offeringCompaniesCounter] = _locAddr;
      offeringCompaniesIDs[_locAddr] = offeringCompaniesCounter;
      offeringCompaniesCounter++;
+     newLOC(msg.sender, _locAddr);
   }
 
   function removeLOC(address _locAddr) onlyAuthorized() execute(Shareable.Operations.editMint) returns (bool) {
     remove(offeringCompaniesIDs[_locAddr]);
     delete offeringCompaniesIDs[_locAddr];
+    remLOC(msg.sender, _locAddr);
     return true;
   }
 
@@ -47,14 +52,17 @@ contract ChronoMint is Managed {
 
   function setLOCStatus(address _LOCaddr, LOC.Status status) onlyAuthorized() execute(Shareable.Operations.editLOC) {
      LOC(_LOCaddr).setStatus(status);
+     updLOCStatus(msg.sender, _LOCaddr);
   }
 
   function setLOCValue(address _LOCaddr, LOC.Setting name, uint value) onlyAuthorized() execute(Shareable.Operations.editLOC) {
     LOC(_LOCaddr).setValue(uint(name),value);
+    updLOCValue(msg.sender, _LOCaddr);  
   }
 
   function setLOCString(address _LOCaddr, LOC.Setting name, bytes32 value) onlyAuthorized() {
     LOC(_LOCaddr).setString(uint(name),value);
+    updLOCValue(msg.sender, _LOCaddr);
   }
 
   function getLOCbyID(uint _id) constant returns(address) {
