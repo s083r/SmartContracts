8a6d47fbf5dfc40257ef647741b9d4cee496e04d
Mike Fluff <mikefluff@Mikes-MacBook-Pro.local>
2017-04-21 17:27:23 +0700

    Small fixes in helpers

--- test/helpers/bytes32.js
+++ test/helpers/bytes32.js
@@ -1,3 +1,6 @@
+const Web3 = require('../../node_modules/web3')
+const web3Location = `http://localhost:8545`
+const web3 = new Web3(new Web3.providers.HttpProvider(web3Location))
 function bytes32(stringOrNumber) {
   var zeros = '000000000000000000000000000000000000000000000000000000000000000';
   if (typeof stringOrNumber === "string") {
