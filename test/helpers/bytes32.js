f06c57779caada66a89e286ae76725dfba6b0f80
Mike Fluff <mikefluff@Mikes-MacBook-Pro.local>
2017-04-21 16:51:47 +0700

    Small fix

--- test/helpers/bytes32.js
+++ test/helpers/bytes32.js
@@ -1,7 +1,8 @@
+const Web3 = require('../../node_modules/web3')
 function bytes32(stringOrNumber) {
   var zeros = '000000000000000000000000000000000000000000000000000000000000000';
   if (typeof stringOrNumber === "string") {
-    return (web3.toHex(stringOrNumber) + zeros).substr(0, 66);
+    return (Web3.toHex(stringOrNumber) + zeros).substr(0, 66);
   }
   var hexNumber = stringOrNumber.toString(16);
   return '0x' + (zeros + hexNumber).substring(hexNumber.length - 1);
