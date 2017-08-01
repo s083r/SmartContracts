6856013b7f0f1f2a173e4a9e6841ebeb02b94e46
Aliaksei Hiatsevich <aliaksei.hiatsevich@gmail.com>
2017-08-01 12:42:24 +0300

    Setup icon to LHT token during migration. (#70)
    Currently uploaded icon info: (https://ipfs.infura.io:5001)
    {"Name":"LHT.svg","Hash":"Qmdhbz5DTrd3fLHWJ8DY2wyAwhffEZG9MoWMvbm3MRwh8V"}

--- migrations/32_deploy_setup_assets.js
+++ migrations/32_deploy_setup_assets.js
@@ -5,12 +5,19 @@ const ChronoBankAssetWithFee = artifacts.require("./ChronoBankAssetWithFee.sol")
 const ChronoBankAsset = artifacts.require("./ChronoBankAsset.sol");
 const AssetsManager = artifacts.require("./AssetsManager.sol");
 const Rewards = artifacts.require("./Rewards.sol");
+const ERC20Manager = artifacts.require("./ERC20Manager.sol");
 const LOCManager = artifacts.require('./LOCManager.sol');
 const Web3 = require("web3");
+const bs58 = require("bs58");
+const BigNumber = require("bignumber.js");
+const Buffer = require("buffer").Buffer;
 
 module.exports = function(deployer,network) {
     const TIME_SYMBOL = 'TIME';
     const LHT_SYMBOL = 'LHT';
+
+    // https://ipfs.infura.io:5001
+    const lhtIconIpfsHash = "Qmdhbz5DTrd3fLHWJ8DY2wyAwhffEZG9MoWMvbm3MRwh8V";
 
     var web3 = new Web3(deployer.provider);
 
@@ -19,6 +26,8 @@ module.exports = function(deployer,network) {
       .then(_rewards => rewards = _rewards)
       .then(() => AssetsManager.deployed())
       .then(_assetsManager => assetsManager = _assetsManager)
+      .then(() => ERC20Manager.deployed())
+      .then(_erc20Manager => erc20Manager = _erc20Manager)
       .then(() => ChronoBankPlatform.deployed())
       .then(_chronoBankPlatform => chronoBankPlatform = _chronoBankPlatform)
       .then(() => ChronoBankAssetProxy.deployed())
@@ -45,7 +54,32 @@ module.exports = function(deployer,network) {
       .then(() => {
         if (network !== "test") {
           return assetsManager.addAsset(chronoBankAssetWithFeeProxy.address, LHT_SYMBOL, LOCManager.address)
+                  .then(() => erc20Manager.getTokenBySymbol.call(LHT_SYMBOL))
+                  .then((asset) => {
+                    return erc20Manager.setToken(asset[0], asset[0], asset[1], asset[2], asset[3], asset[4], ipfsHashToBytes32(lhtIconIpfsHash), asset[6])})
+                      .then(() => erc20Manager.getTokenBySymbol.call(LHT_SYMBOL))
+                      .then((asset) => {
+                        if (lhtIconIpfsHash != bytes32ToIPFSHash(asset[5])) {
+                            console.error("Error: can't setup LHT icon");
+                        }
+                  })
         }
       })
       .then(() => console.log("[MIGRATION] [32] Setup Assets: #done"))
+}
+
+// Util function
+// TODO: @ahiatsevich: copy-paste from
+// ChronoBank/ChronoMint/src/utils/Web3Converter.js
+
+function bytes32ToIPFSHash (bytes) {
+  if (/^0x0{63}[01]$/.test(`${bytes}`)) {
+    return ''
+  }
+  const str = Buffer.from(bytes.replace(/^0x/, '1220'), 'hex')
+  return bs58.encode(str)
+}
+
+function ipfsHashToBytes32 (value) {
+  return `0x${Buffer.from(bs58.decode(value)).toString('hex').substr(4)}`
 }
