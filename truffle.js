690e8df10b2bb33f5b1fba21bf4765b682cbf8c3
s083r <danila.zubkov@gmail.com>
2017-08-08 16:40:29 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -4,7 +4,13 @@ networks: {
     kovan:{
       network_id:42,
       gas: 4700000
-    }
+    },
+    test: {
+      network_id: 424242,
+      host: 'localhost',
+      port: 8545,
+      gas: 4700000
+    },
   },
   migrations_directory: './migrations'
 }
