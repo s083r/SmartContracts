b830b9d7d09ead563b7bfa706aa49989e005466f
s083r <danila.zubkov@gmail.com>
2017-08-08 16:59:30 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -3,12 +3,14 @@ module.exports = {
 networks: {
     kovan:{
       network_id:42,
+      host: 'kovan.chronobank.io',
+      port: 443,
       gas: 4700000
     },
     test: {
       network_id: 424242,
-      host: 'kovan.chronobank.io',
-      port: 443,
+      host: 'localhost',
+      port: 8545,
       gas: 4700000
     },
   },
