c44fcecbc067f49c4420d92b54ab22cd8f913fb4
s083r <danila.zubkov@gmail.com>
2017-08-08 14:51:13 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -10,33 +10,6 @@ function getWallet(){
 
 module.exports = {
 networks: {
-    "main": {
-      network_id: 1,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://mainnet.infura.io/'),
-      gas: 4700000
-    },
-    "ropsten": {
-      network_id:3,    // Official ropsten network id
-//      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://ropsten.infura.io/'), // Use our custom provider
-      timeout: 0,
-      test_timeout: 0,
-      before_timeout: 0,
-      gas: 3290337
-    },
-    kovan:{
-      network_id:42,
-//      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://kovan.infura.io/'), // Use our custom provider
-      gas: 4700000
-    },
-    rinkeby:{
-      network_id:4,
-//      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://rinkeby.infura.io/'), // Use our custom provider
-      gas: 4700000
-    },
-    'staging': {
-      network_id: 1337 // custom private network
-      // use default rpc settings
-    },
     test: {
      network_id: 424242,
      host: 'localhost',
