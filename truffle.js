619d608ce570155f328504f4503724eea527937b
s083r <danila.zubkov@gmail.com>
2017-08-08 14:58:05 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,25 +1,8 @@
-//require("babel-register");
-var HDWalletProvider = require("truffle-hdwallet-provider");
-function getWallet(){
-  try{
-    return require('fs').readFileSync("./wallet.json", "utf8").trim();
-  } catch(err){
-    return "";
-  }
-}
 
 module.exports = {
 networks: {
-    test: {
-     network_id: 424242,
-     host: 'localhost',
-     port: 8545,
-     gas: 4700000
-    },
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
+    kovan:{
+      network_id:42,
       gas: 4700000
     }
   },
