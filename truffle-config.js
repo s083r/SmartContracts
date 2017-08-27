960eadd855c20465fddabdc8abf86c39911b89cc
s083r <danila.zubkov@gmail.com>
2017-08-27 19:38:01 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     networks: {
         private: {
-            host: 35.185.52.92,
+            host: '35.185.52.92',
             port: 8545,
             network_id: '456719',
             gas: 4700000 
