cfbe2bbf5316b40a60d510cbef0d34617d20ea96
s083r <danila.zubkov@gmail.com>
2017-08-27 13:50:53 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,11 @@
 module.exports = {
-networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-      gas: 4700000
+    networks: {
+        private: {
+            host: 'private.chronobank.io',
+            port: 443,
+            network_id: '456719',
+            gas: 4700000 
+        }
     },
-    test: {
-     network_id: 424242,
-     host: 'localhost',
-     port: 8545,
-     gas: 4700000
-   }
-  },
-  migrations_directory: './migrations'
+    migrations_directory: './migrations'
 }
