7d886378f9209b2ec328a9c28506882579f3ac6f
s083r <danila.zubkov@gmail.com>
2017-08-28 10:53:18 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -10,44 +10,33 @@ function getWallet(){
 
 module.exports = {
 networks: {
-    "main": {
+    "mainnet": {
       network_id: 1,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://mainnet.infura.io/'),
-      gas: 3290337
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://mainnet.chronobank.io/'),
+      gas: 4700000
     },
     "ropsten": {
       network_id:3,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://ropsten.infura.io/'), // Use our custom provider
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://ropsten.chronobank.io/'),
       timeout: 0,
       test_timeout: 0,
       before_timeout: 0,
       gas: 3290337
     },
-    kovan:{
-      network_id:42,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://kovan.infura.io/'), // Use our custom provider
-      gas: 4700000
-    },
     rinkeby:{
       network_id:4,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://rinkeby.infura.io/'), // Use our custom provider
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://rinkeby.chronobank.io/'),
       gas: 4700000
     },
-    'staging': {
-      network_id: 1337 // custom private network
-      // use default rpc settings
+    kovan:{
+      network_id:42,
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://kovan.chronobank.io/'),
+      gas: 4700000
     },
-    test: {
-     network_id: 424242,
-     host: 'localhost',
-     port: 8545,
-     gas: 4700000
-    },
-    develop: {
-     host: 'private.chronobank.io',
-     port: 8545,
-     network_id: '456719',
-     gas: 4700000 
+    private: {
+      network_id: '456719', 
+      provider: new HDWalletProvider('','https://private.chronobank.io/'),
+      gas: 4700000
     }
   },
   migrations_directory: './migrations'
