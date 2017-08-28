05ce78e5e9df29b327001073b46a9bfa48a4cded
s083r <danila.zubkov@gmail.com>
2017-08-28 10:53:45 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,43 +1,17 @@
-//require("babel-register");
-var HDWalletProvider = require("truffle-hdwallet-provider");
-function getWallet(){
-  try{
-    return require('fs').readFileSync("./wallet.json", "utf8").trim();
-  } catch(err){
-    return "";
-  }
-}
-
 module.exports = {
 networks: {
-    "mainnet": {
-      network_id: 1,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://mainnet.chronobank.io/'),
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
       gas: 4700000
     },
-    "ropsten": {
-      network_id:3,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://ropsten.chronobank.io/'),
-      timeout: 0,
-      test_timeout: 0,
-      before_timeout: 0,
-      gas: 3290337
-    },
-    kovan:{
-      network_id:42,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://kovan.chronobank.io/'),
-      gas: 4700000
-    },
-    rinkeby:{
-      network_id:4,
-      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://rinkeby.chronobank.io/'),
-      gas: 4700000
-    },
-    private: {
-      network_id: '*', 
-      provider: new HDWalletProvider('','https://private.chronobank.io/'),
-      gas: 4700000
-    }
+    test: {
+     network_id: 424242,
+     host: 'localhost',
+     port: 8545,
+     gas: 4700000
+   }
   },
   migrations_directory: './migrations'
 }
